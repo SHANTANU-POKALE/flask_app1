ad3edafeba5c5016d407ed362c6727d433e51694
shantanu pradip pokale <shantanupokale009@gmail.com>
2024-08-05 13:45:22 +0530

    pass set

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 465
 app.config["MAIL_USE_SSL"] = True
 app.config["MAIL_USERNAME"] = "festivalphotos004@gmail.com"
-app.config["MAIL_PASSWORD"] = "xegm jxoj imgo phsn"
+app.config["MAIL_PASSWORD"] = PASSWORD
 
 db = SQLAlchemy(app)
 
